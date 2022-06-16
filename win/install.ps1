3948c1ca142516175011bf19569ce0d283e35f0e
CH3CHO <ch3cho@qq.com>
2022-06-16 15:00:50 +0800

    Update.

--- win/install.ps1
+++ win/install.ps1
@@ -1,3 +1,18 @@
 Start http://github.com/PowerShell/PowerShell/releases/latest
+Start https://github.com/genzj/pybingwallpaper/releases/latest
+Start https://adoptium.net/temurin/releases?version=8
+Start https://adoptium.net/temurin/releases?version=11
 
-set-executionpolicy remotesigned -s currentuser
+$WORKING_DISK = "D:\"
+
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser;
+$env:SCOOP = $WORKING_DISK + "Tools\scoop"
+[Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
+iwr -useb get.scoop.sh | iex
+scoop update
+
+scoop bucket add extras
+
+scoop install autohotkey
+scoop install gsudo
+scoop install dnGrep
